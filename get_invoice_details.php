2abcb3a0b896213f645cf1ad69431838cde50b11
BRkavin <109970394+BRkavin@users.noreply.github.com>
2024-04-24 16:10:07 +0530

    Update get_invoice_details.php

--- get_invoice_details.php
+++ get_invoice_details.php
@@ -7,7 +7,7 @@ if (isset($_POST['contactNumber']) && isset($_POST['studentName'])) {
     $studentName = mysqli_real_escape_string($conn, $_POST['studentName']);
 
     // Query to get invoice details sorted by last updated time and date
-    $query = "SELECT due_amount, grand_total FROM invoices WHERE contact_number = '$contactNumber' AND student_name ='$studentName' ORDER BY DATE_FORMAT(CONCAT(invoice_date, ' ', time), '%Y-%m-%d %H:%i:%s') DESC LIMIT 1";
+    $query = "SELECT due_amount, grand_total FROM invoices WHERE contact_number = '$contactNumber' AND student_name ='$studentName' ORDER BY time DESC LIMIT 1";
     
     $result = $conn->query($query);
 
